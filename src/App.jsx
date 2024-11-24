e6f905ca4f18884c612a1a8c709bb3cb15bfb813
Syed Khadeer <khadeer1206@gmail.com>
2024-11-24 20:18:56 +0100

    feat: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import { Navbar, HeroSection, FacilitiesSection, TestimonialSection, LocationSection, FooterSection, PopularHotelSection, HistorySection } from "./components"
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <HeroSection/>
+      <PopularHotelSection/>
+      <FacilitiesSection/>
+      <HistorySection/>
+      <TestimonialSection/>
+      <LocationSection/>
+      <FooterSection/>
+    </div>
+    
+  )
+}
+export default App
